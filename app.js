a33f613e9ac323f91960c5b798e927cfe74ac7ba
georgezappa <georgezappa@gmail.com>
2023-03-14 11:47:08 -0500

    still more cleanup

---
+++ app.js
@@ -0,0 +1,56 @@
+import createError from "http-errors";
+import express, { json, urlencoded } from "express";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import sassMiddleware from "node-sass-middleware";
+import indexRouter from "./routes/indexRouter.js";
+import usersRouter from "./routes/usersRouter.js";
+import contactRouter from "./routes/contactRouter.js";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: join(__dirname, "public"),
+  dest: join(__dirname, "public"),
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/contact", contactRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+  next();
+});
+
+app.use(logger("post end: \n\n"));
+
+export default app;
---
+++ bin/www.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+/**
+ * Module dependencies.
+ */
+import app from "../app.js";
+import { createServer } from "http";
+import { env } from "node:process";
+import { exit } from "node:process";
+
+/**
+ * Get port from environment and store in Express.
+ */
+var port = normalizePort(env.PORT || "8080");
+app.set("port", port);
+/**
+ * Create HTTP server.
+ */
+var server = createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+  if (error.syscall !== "listen") {
+    console.log("error.syscall is ", error.syscall );
+    throw error;
+  }
+
+  var bind = typeof port === "string"
+    ? `Pipe ${port}`
+    : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string"
+    ? `pipe ${addr}`
+    : `port ${addr.port}`;
+  //log(`Listening on ${bind}`);
+  console.log(`I"m Listening on port ${bind}`);
+}
